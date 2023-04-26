c364177b58d536502d2c341913b43219ee29edf9
MilanMj11 <mujdar.milan@gmail.com>
2023-04-27 02:39:44 +0300

    Finalizat
    yupi

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 using namespace std;
 
 int main() {
-    vector<Humvee> Humvee_list;
     Depot depozit;
     int n,nr;
     cout << "Cate vehicule doriti sa introduceti ?\n";
@@ -27,8 +26,20 @@ int main() {
         }
     }
     std::vector<std::shared_ptr<Vehicle>> Lista = depozit.getVList();
+
+    /// AICI PUTEM SA LUCRAM CU VEHICULELE , ADAUGAM ATRIBUTE , BLA BLA
+
+    /// ________________________________________________________________
+
+    cout << "Depozitul de vehicule contine: \n";
     for(auto it: Lista){
         cout << *it << '\n';
     }
+    cout << '\n';
+    /// pentru exceptie , demonstratie ca merge :
+    Humvee MyH("M220 TOW",70,2016,3,10,5,1,0);
+    MyH.AddStorage(4); /// pot sa adaug 4 , pentru ca 5+4 = 9 < 10
+    MyH.AddStorage(2); /// nu mai pot sa adaug , pentru ca 9+2 = 11 > 10
+    /// asa ca arunca exceptie;
     return 0;
 }
